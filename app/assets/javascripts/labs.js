5a9a2ae24771f20d1f90a65f7ad27fb5e9c4caa2
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-28 12:02:06 -0400

    style drop down menu of lab files to view on lab show use slowToggle to slide file selection in and out, style toggle button

--- app/assets/javascripts/labs.js
+++ app/assets/javascripts/labs.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
   addFileSelectListener();
+  $("[data-id='lab-files']").hide();
 })
 
 function addFileSelectListener(){
   $("[data-id='lab-files-down']").on("click", function(e){
-    debugger;
+    // debugger;
     e.preventDefault;
     e.stopPropagation;
-    var btn = e.toElement;
-    var parent = btn.parentElement;
-    btn.remove();
-    parent.append('button class="btn btn-info" data-id="lab-files-down"><span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></button>')
-    $("[data-id='lab-files']").toggleClass("hidden");
+    // debugger;
+    $("[data-id='lab-files-down']").toggleClass('rotate');
+    $("[data-id='lab-files-down']").toggleClass('rotate-reset');
+    // this.style.transform = "rotate(180deg)";
+    
+    // debugger;
+    // var btn = e.toElement;
+    // btn.rotate();
+    // var parent = btn.parentElement;
+    // btn.remove();
+    // parent.append('button class="btn btn-info" data-id="lab-files-down"><span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></button>')
+    $("[data-id='lab-files']").slideToggle("slow")
     // var studentPRFiles = btn.getAttribute('data-id')
     // $("." + studentPRFiles).toggleClass('hidden');
   })
 }
+
