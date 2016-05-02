42b5edaf5b76728163df932c959167705ea1f957
Chris Aporta <christopher.aporta@gmail.com>
2016-05-01 20:34:56 -0400

    build out basic controller && view logic for search add basic styles && seed db with Student record

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,13 @@
 class SearchesController < ApplicationController
 
   def new
-    binding.pry
+    unless params[:query].empty? || params[:query] == " "
+      if params[:type] == 'students'
+        @students = Student.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+      else
+        @labs = Lab.where('name ILIKE ? AND cohort_id = ?', "%#{params[:query]}%", params[:cohort][:id])
+      end
+    end
     render 'new'
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Student.create(first_name: 'Chris', last_name: 'Aporta', github_username: 'caporta', cohort_id: '1')
