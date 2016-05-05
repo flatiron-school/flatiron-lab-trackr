2f5ad5292784df6ff2c1428f01419d7ece912899
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-05 08:16:14 -0400

    switch to preform now for adding new prs from webhooks payload job

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,7 +8,7 @@ class WebhooksController < ApplicationController
       return
     else
       pull_request = Adapters::GitHubWrapper.new.create_or_update_pr_from_webhook(params[:pull_request])
-      WebHooksPullRequestUpdaterJob.perform_later(pull_request)
+      WebHooksPullRequestUpdaterJob.perform_now(pull_request)
     end
   end
 end
