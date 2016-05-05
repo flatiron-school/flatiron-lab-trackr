b1b397b3b8e22c4f52ad0c1a2358b7845ea2b9c0
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-05 08:14:43 -0400

    remove binding.pry from webhooks controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,7 +8,6 @@ class WebhooksController < ApplicationController
       return
     else
       pull_request = Adapters::GitHubWrapper.new.create_or_update_pr_from_webhook(params[:pull_request])
-      binding.pry
       WebHooksPullRequestUpdaterJob.perform_later(pull_request)
     end
   end
