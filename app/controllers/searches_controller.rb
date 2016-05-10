c7e1987cb6d72e00b8a10cf60e490506501a2d03
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-05-09 21:03:30 -0400

    refactor searches controller code into Search instance methods #execute method contains logic around lab vs. student search, enacts search and sets results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,14 +2,16 @@ class SearchesController < ApplicationController
 
   def new
     @search = Search.new(type: params[:type], query: params[:query])
-    cohort_id = params[:cohort][:id]
-    if @search.valid?
-      if @search.type == 'students'
-        @search.results = Student.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{@search.query}%", "%#{@search.query}%")
-      else
-        @search.results = Lab.where('name ILIKE ? AND cohort_id = ?', "%#{@search.query}%", cohort_id)
-      end
-    end
+    @search.execute(params[:cohort][:id]) if @search.valid?
+    # cohort_id = params[:cohort][:id]
+    # if @search.valid?
+    #   @search.execute(params[:cohort][:id])
+    #   # if @search.type == 'students'
+    #   #   @search.results = Student.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{@search.query}%", "%#{@search.query}%")
+    #   # else
+    #   #   @search.results = Lab.where('name ILIKE ? AND cohort_id = ?', "%#{@search.query}%", cohort_id)
+    #   # end
+    # end
     render 'new'
   end
 
--- app/models/search.rb
+++ app/models/search.rb
@@ -7,4 +7,29 @@ class Search
     results && !results.empty?
   end
 
+  def execute(cohort_id)
+    if self.type == 'students'
+      student_search
+    else
+      lab_search
+    end
+  end
+
+  def student_search
+    self.results = Student.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{self.query}%", "%#{self.query}%")
+  end
+
+  def lab_search
+    self.results = Lab.where('name ILIKE ? AND cohort_id = ?', "%#{self.query}%", cohort_id)
+  end
+
+  def for_students?
+    self.type == "students"
+  end
+
+  def for_labs?
+    self.type == "labs"
+  end
+
+
 end
