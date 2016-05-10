1b2192a9cf673e7129dd31521ec0211cf570f65e
Chris Aporta <christopher.aporta@gmail.com>
2016-05-09 23:34:31 -0400

    refactor cohort_id into attr & utilize implicit self in model

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,8 @@
 class SearchesController < ApplicationController
 
   def new
-    @search = Search.new(type: params[:type], query: params[:query])
-    @search.execute(params[:cohort][:id]) if @search.valid?
-    # cohort_id = params[:cohort][:id]
-    # if @search.valid?
-    #   @search.execute(params[:cohort][:id])
-    #   # if @search.type == 'students'
-    #   #   @search.results = Student.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{@search.query}%", "%#{@search.query}%")
-    #   # else
-    #   #   @search.results = Lab.where('name ILIKE ? AND cohort_id = ?', "%#{@search.query}%", cohort_id)
-    #   # end
-    # end
+    @search = Search.new(type: params[:type], query: params[:query], cohort_id: params[:cohort][:id])
+    @search.execute if @search.valid?
     render 'new'
   end
 
